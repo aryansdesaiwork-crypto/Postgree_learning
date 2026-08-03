aff9b1fca0abd7c6caf1a7e7f28a4f89eab519ce
aryansdesaiwork-crypto <aryansdesai.work@gmail.com>
2026-08-03 21:37:57 +0530

    Add files via upload

---
+++ pg1.sql
@@ -0,0 +1,23 @@
+INSERT INTO students (id,name,age)
+VALUES (1,'Aryan',20);
+
+
+TRUNCATE TABLE students;
+
+SELECT * FROM students;
+
+INSERT INTO students (id,name,age)
+VALUES (2,'ARYAN',21);
+SELECT * FROM students;
+
+SELECT * FROM students WHERE age = 21;
+
+INSERT INTO students (id,name,age)
+VALUES (3,'Kohli',38);
+INSERT INTO students(id,name,age)
+VALUES(4,'rohit',40);      
+
+Select *from students;
+
+SELECT * FROM students Where age>30;
+SELECT * FROM students Where age<40;
